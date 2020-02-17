1d0c36b06ce5ab8326aea6ed5f4f55b771722105
SupLinux <30951604+SupLinux@users.noreply.github.com>
2020-02-17 14:13:21 +0800

    Update gen_report.sh

--- openshift/gen_report.sh
+++ openshift/gen_report.sh
@@ -4,7 +4,7 @@ if [ $# -lt 2 ]; then
     echo "Please input parameters are not enough, please check them"
     exit 1
 fi
-echo "1111111111"
+
 WORKSPACE=$1
 BUILD_NUMBER=$2
 
@@ -13,7 +13,6 @@ echo $WORKSPACE
 ls $WORKSPACE
 # depoly perfchart which uses to generate test report
 filter=`date +%s`
-# fileter="1581824008"
 oc process -f perfchart_dc_template.yaml -p FILTER=$filter | oc create -f -
 
 # check if pod is running 
@@ -26,21 +25,18 @@ while [ $count -lt 60 ]; do
     echo "waiting for report-engine ready..."
     sleep 3 
 done
-echo "2222222222222"
 
 # copy test results to perfchart pod
 perfchart_pod=`oc get pod  | grep "perfchart-$filter" | awk '{print $1}'`
 oc cp $WORKSPACE/perf-output/builds/$BUILD_NUMBER/rawdata/*.jtl $perfchart_pod:/tmp/
 
-echo "123456789"
 #generate test report
 oc exec -ti $perfchart_pod -- perfcharts gen perf-general -d /tmp/report -o /tmp/report/mono_report.html -z UTC /tmp/
 
 #copy reports back
 echo "copy reports back"
-ls
 oc rsync $perfchart_pod:/tmp/report $WORKSPACE/perf-output/builds/$BUILD_NUMBER/
-pwd 
-ls $WORKSPACE/perf-output/builds/$BUILD_NUMBER/
+oc rsync "$WORKSPACE/perf-output/builds/$BUILD_NUMBER/report"  "jenkins-2-h6pbr:/var/lib/jenkins/jobs/perf-pipline/builds/$BUILD_NUMBER/perfcharts/"
+
 #clean all pods
-# oc delete all -l jmeter_report="perfchart-$filter"
+oc delete all -l jmeter_report="perfchart-$filter"
